419dd490256f51a8f4759b652d630c6a49a3aa57
Dolaan <116778198+Dembirel@users.noreply.github.com>
2025-01-27 17:08:01 +0700

    Update main.go

--- main.go
+++ main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
+	var a = Graph{make(map[int][]int)}
+	var b = Graph{make(map[int][]int)}
+	var c = Graph{make(map[int][]int)}
+	var d = Graph{make(map[int][]int)}
+	a.AddEdge(1, 2, false)
+	b.AddEdge(2, )
+	c.AddEdge()
+	d.AddEdge()
+	if HasEdge(&a, 1, 2) {
+
+	}
 	fmt.Println("Hello world!")
 }
